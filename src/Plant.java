f98882a4539b38fd6caf9aac39ab7cb8e4fecf95
Zack Gacnik <zgacnik@carroll.edu>
2026-02-17 17:30:11 -0700

    Worker and Plant finished

--- src/Plant.java
+++ src/Plant.java
@@ -1,98 +1,121 @@
 public class Plant implements Runnable {
-    // How long do we want to run the juice processing
-    public static final long PROCESSING_TIME = 5 * 1000;
 
+    public static final long PROCESSING_TIME = 5 * 1000;
     private static final int NUM_PLANTS = 2;
+    private static final int NUM_WORKERS = 2;
+
+    public final int ORANGES_PER_BOTTLE = 3;
+
+    private final Thread thread;
+
+    private int orangesProvided;
+    private int orangesProcessed;
+
+    private volatile boolean timeToWork;
+
+    private BlockingMailbox mailbox;
+    private Worker[] workers;
 
     public static void main(String[] args) {
-        // Startup the plants
+
         Plant[] plants = new Plant[NUM_PLANTS];
+
         for (int i = 0; i < NUM_PLANTS; i++) {
-            plants[i] = new Plant(1);
+            plants[i] = new Plant(i + 1);
             plants[i].startPlant();
         }
 
-        // Give the plants time to do work
         delay(PROCESSING_TIME, "Plant malfunction");
 
-        // Stop the plant, and wait for it to shutdown
-        for (Plant p : plants) {
+        for (Plant p : plants)
             p.stopPlant();
-        }
-        for (Plant p : plants) {
-            p.waitToStop();
-        }
 
-        // Summarize the results
+        for (Plant p : plants)
+            p.waitToStop();
+
         int totalProvided = 0;
         int totalProcessed = 0;
         int totalBottles = 0;
         int totalWasted = 0;
+
         for (Plant p : plants) {
             totalProvided += p.getProvidedOranges();
             totalProcessed += p.getProcessedOranges();
             totalBottles += p.getBottles();
             totalWasted += p.getWaste();
         }
-        System.out.println("Total provided/processed = " + totalProvided + "/" + totalProcessed);
+
+        System.out.println("Total provided/processed = "
+                + totalProvided + "/" + totalProcessed);
         System.out.println("Created " + totalBottles +
                 ", wasted " + totalWasted + " oranges");
     }
 
     private static void delay(long time, String errMsg) {
-        long sleepTime = Math.max(1, time);
         try {
-            Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            System.err.println(errMsg);
+            Thread.sleep(Math.max(1, time));
+        } catch (InterruptedException ignored) {}
+    }
+
+    Plant(int threadNum) {
+
+        orangesProvided = 0;
+        orangesProcessed = 0;
+
+        thread = new Thread(this, "Plant[" + threadNum + "]");
+
+        mailbox = new BlockingMailbox();
+
+        workers = new Worker[NUM_WORKERS];
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            workers[i] = new Worker(this, mailbox, i + 1);
         }
     }
 
-    public final int ORANGES_PER_BOTTLE = 3;
-
-    private final Thread thread;
-    private int orangesProvided;
-    private int orangesProcessed;
-    private volatile boolean timeToWork;
-
-    Plant(int threadNum) {
-        orangesProvided = 0;
-        orangesProcessed = 0;
-        thread = new Thread(this, "Plant[" + threadNum + "]");
+    public String getThreadName() {
+        return thread.getName();
     }
 
     public void startPlant() {
         timeToWork = true;
+
+        for (Worker w : workers)
+            w.startWorker();
+
         thread.start();
     }
 
     public void stopPlant() {
         timeToWork = false;
+
+        for (Worker w : workers)
+            w.stopWorker();
     }
 
     public void waitToStop() {
         try {
             thread.join();
-        } catch (InterruptedException e) {
-            System.err.println(thread.getName() + " stop malfunction");
-        }
+
+            for (Worker w : workers)
+                w.waitToStop();
+
+        } catch (InterruptedException ignored) {}
     }
 
     public void run() {
-        System.out.print(Thread.currentThread().getName() + " Processing oranges");
+        System.out.print(Thread.currentThread().getName() + " Processing oranges\n");
+
         while (timeToWork) {
-            processEntireOrange(new Orange());
+            mailbox.put(new Orange());
             orangesProvided++;
             System.out.print(".");
         }
-        System.out.println("");
+
+        System.out.println();
         System.out.println(Thread.currentThread().getName() + " Done");
     }
 
-    public void processEntireOrange(Orange o) {
-        while (o.getState() != Orange.State.Bottled) {
-            o.runProcess();
-        }
+    public synchronized void incrementProcessed() {
         orangesProcessed++;
     }
 
--- src/Worker.java
+++ src/Worker.java
@@ -1,6 +1,49 @@
 public class Worker implements Runnable {
 
-    public synchronized void run() {
+    private final Thread thread;
+    private final Plant plant;
+    private final BlockingMailbox mailbox;
 
+    private volatile boolean timeToWork;
+
+    public Worker(Plant plant, BlockingMailbox mailbox, int num) {
+        this.plant = plant;
+        this.mailbox = mailbox;
+        this.thread = new Thread(this,
+                plant.getThreadName() + "-Worker[" + num + "]");
     }
+
+    public void startWorker() {
+        timeToWork = true;
+        thread.start();
+    }
+
+    public void stopWorker() {
+        timeToWork = false;
+    }
+
+    public void waitToStop() {
+        try {
+            thread.join();
+        } catch (InterruptedException ignored) {}
+    }
+
+    public void run() {
+
+        System.out.println(thread.getName() + " started.");
+
+        while (timeToWork || !mailbox.isEmpty()) {
+
+            Orange o = mailbox.get();
+
+            while (o.getState() != Orange.State.Bottled) {
+                o.runProcess();
+            }
+
+            plant.incrementProcessed();
+        }
+
+        System.out.println(thread.getName() + " stopped.");
+    }
+
 }
